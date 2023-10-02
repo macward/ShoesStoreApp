76a5034c688a06e36356f1444482a6cb0cab6acc
Max Ward <ward.maximiliano@gmail.com>
2023-10-02 18:59:00 -0300

    Initial Commit

---
+++ ShoeStoreWithRedux/ShoeStoreWithReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeStoreWithReduxApp.swift
+//  ShoeStoreWithRedux
+//
+//  Created by Max Ward on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeStoreWithReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
