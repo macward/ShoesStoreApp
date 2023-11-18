a0e5d23c4b78bb06a6f882782578331a30583edd
Max Ward <ward.maximiliano@gmail.com>
2023-11-17 23:34:44 -0300

    checkout module

---
+++ ShoeStoreWithRedux/Presentations/Features/Checkout/Sources/Checkout/Screen/CheckoutScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Max Ward on 17/11/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
